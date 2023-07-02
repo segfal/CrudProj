6127fd3caa27d9e5aa885464b69784e37ac23664
Hesham <heshamsharafaldin@gmail.com>
2023-07-02 04:07:30 +0300

    Fixed delete student function

--- backend/Routes/students.js
+++ backend/Routes/students.js
@@ -1,76 +1,59 @@
 const express = require("express");
-const router = require('express').Router();
-const {Student, findStudents} = require('../DB/Models/students');
-
+const router = require("express").Router();
+const { Student, findStudents } = require("../DB/Models/students");
 
 // Fetch all students DONE
-router.get('/', async (req, res,next) => {
-    try{
-        //if find all is null, send 404
-        
-        const allStudents = await findStudents();
-        
-        allStudents ? res.status(200).json(allStudents) : res.status(404).send("No students found");
-    } catch(error) {
-        next(error);
-    }
+router.get("/", async (req, res, next) => {
+  try {
+    //if find all is null, send 404
 
+    const allStudents = await findStudents();
+
+    allStudents
+      ? res.status(200).json(allStudents)
+      : res.status(404).send("No students found");
+  } catch (error) {
+    next(error);
+  }
 });
-
 
 // add new student DONE
-router.post('/addstudent', async (req, res, next) => {
-    try{
-        //console.log(req.body);
-        //console.log(req.body.gpa);
+router.post("/addstudent", async (req, res, next) => {
+  try {
+    //console.log(req.body);
+    //console.log(req.body.gpa);
 
-        if(req.body.gpa < 0.00 || req.body.gpa > 4.00){// if gpa is not between 0 and 4
-            res.status(500);
-            console.log(res.status(500)); //test what's happening
-            // 500 sends to the user :"thats not good"
-            return false;
-        }
-        const newStudent = await Student.create(req.body); //create new student
-    
-        res.status(201).json(newStudent); //return json of new student
-    } catch (error){
-        console.log("An error occured", error); // if error, console log it
+    if (req.body.gpa < 0.0 || req.body.gpa > 4.0) {
+      // if gpa is not between 0 and 4
+      res.status(500);
+      console.log(res.status(500)); //test what's happening
+      // 500 sends to the user :"thats not good"
+      return false;
     }
+    const newStudent = await Student.create(req.body); //create new student
+
+    res.status(201).json(newStudent); //return json of new student
+  } catch (error) {
+    console.log("An error occured", error); // if error, console log it
+  }
 });
 
-
-//TODO: delete student 
-router.delete('/deletestudent/:id', async (req, res, next) => {
-    try{
-        const user = await user.findStudents({
-            where: {
-                id: req.body,
-            }
-        });
-
-        if (user) {
-            console.log('User exists.');
-          } else {
-            console.log('User does not exist.');
-          }
-
-    } catch(error){
-        console.log("An Error has occured" , error);
-    }
-
+//TODO: delete student
+router.delete("/deletestudent", async (req, res, next) => {
+  try {
+    console.log(req.body.id);
+    const deleteStudent = await Student.destroy({
+      where: {
+        firstName: req.body.firstName,
+      },
+    });
+    res.status(200).json(deleteStudent);
+  } catch (error) {
+    console.log("Delete Failed");
+  }
 });
-
-
-
-
 
 //TODO: update student
-router.put('/updatestudent/:id', async (req, res, next) => {
-
-});
-
-
-
-
+router.put("/updatestudent/:id", async (req, res, next) => {});
 
 module.exports = router;
