215ba5a4f8445514c7f685b76710d0c5a6f533ca
Hesham <heshamsharafaldin@gmail.com>
2023-07-02 07:25:59 +0300

    added delete student component, though it's not deleting any students from database

--- frontend/src/pages/SingleStudent.jsx
+++ frontend/src/pages/SingleStudent.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SingleStudent = () => {
+  const navigate = useNavigate();
+  
+  const handleAdd = () => {
+    let path = `/deleteStudent/*`;
+    navigate(path);
+};
+  
   return (
     <div>
       <h1>Welcome to the Single Student View</h1>
-      <button type="button" className="btn btn-danger">
+      <button type="button" className="btn btn-danger" onClick={handleAdd}> 
         Delete
       </button>
     </div>
