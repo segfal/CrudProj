f5ac3a7e94f2e606364249019461637d19b85ad7
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-03 13:20:06 -0400

    Clean up Single Campus page

--- frontend/src/pages/SingleCampus.jsx
+++ frontend/src/pages/SingleCampus.jsx
@@ -3,26 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { fetchCampuses } from "../redux/Campus.actions";
 
-
-//how to pass props from single campus using navigate?
-
-
 const SingleCampus = (props) => {
-  //trying this:
   const { campusId } = useParams(); //the params allows u to access the campusId from URL parameters
   const [singleCampus, setSingleCampus] = useState([]); //this is the data that we get from the backend
 
-  //what the useParams thing does is basically, since we have the route:
-  // <Route path="/SingleCampus/:campusId" component={SingleCampus} />
- //then the params will be referring to the part that says :campusId, aka the tab that this leads to
-// backend: req.params
- const campus = fetchCampuses();
- console.log(campus);
-
-
- console.log(campusId);
- const campusUrl = `http://localhost:8080/routes/campuses/SingleCampus/${campusId}`;
- useEffect(()=>{
+  const campus = fetchCampuses();
+  const campusUrl = `http://localhost:8080/routes/campuses/SingleCampus/${campusId}`;
+  useEffect(()=>{
     fetch(campusUrl)
     .then((res) => res.json())
     .then((data) => { //data is the response from the backend
@@ -30,25 +17,22 @@ const SingleCampus = (props) => {
       setSingleCampus(data); //this is the data that we get from the backend
     })
     .catch((err) => console.log(err));
- },[]);
+  },[]);
 
 
   return (
     <div>
-      <h1>Welcome to the Single Campus View</h1>
+      <h1>Learn more about {singleCampus.name}</h1>
+      <img src={singleCampus.imageUrl} alt="campus image" />
+      <h2> Campus Name: {singleCampus.name} </h2> 
+      <h2> Campus Address: {singleCampus.address} </h2>
+      <h2> Campus Description: {singleCampus.description} </h2>
       <button type="button" className="btn btn-success">
         Edit
       </button>
       <button type="button" className="btn btn-danger">
         Delete
       </button>
-      <h2> Campus ID: {campusId} </h2>
-      <h2> Campus Name: {singleCampus.name} </h2> 
-      <h2> Campus Address: {singleCampus.address} </h2>
-      <h2> Campus Description: {singleCampus.description} </h2>
-      <img src={singleCampus.imageUrl} alt="campus image" />
-      
-
     </div>
   );
 };
