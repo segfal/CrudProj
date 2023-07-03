47e094023fe4ccd4ed40350c5c1b2a51ed28a747
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-02 22:01:00 -0400

    Fix bug with student image not displaying

--- frontend/src/components/StudentItems.jsx
+++ frontend/src/components/StudentItems.jsx
@@ -5,7 +5,7 @@ export default function StudentItem(props) {
     if (props.list && props.list.length > 0) {
       return props.list.map((student) => (
         <div key={student.firstName}>
-          <img src={student.imageURL} alt={student.name} />
+          <img src={student.imageUrl} alt={student.name} />
           <h2>{student.firstName}</h2>
           <h2>{student.lastName}</h2>
           <h2>{student.email}</h2>
