fcf1f3962c9c17b2904f5e6d808f5d089134811c
Huda Ayaz <hudaayaz21@gmail.com>
2023-07-06 16:06:42 -0400

    Fix home page structure

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,16 +5,18 @@ import Campuses from "./Campuses";
 
 const Home = () => {
   return (
-    <div className="container"> 
-      <p>
-        <h1 className="heading">Welcome to the homepage</h1>
-        <p className="paragraph">Write more here.</p>
-      </p>
-        <div className="button-container" style={{marginTop:"40vh", borderRadius:"10px", backgroundColor:""}}>
-          <Link to="/campuses" className="Homebutton" id="gradient-button">Campuses Directory</Link>
-          <Link to="/students" className="Homebutton" id="gradient-button">Students Directory</Link>
-          {/* <Link to="/" className="Homebutton" id="gradient-button">Home Page</Link> */}
+    <div className="bg-img">
+      <div className="overlay">
+        <div className="landing__container">
+          <h1 className="heading">Welcome to The Campus Student Directory</h1>
+          <p className="paragraph">Keep track of colleges, students, and their associations on this site, where you can create, read, update, or delete campus and student information.</p>
+          <div className="button-container">
+            <Link to="/campuses" className="Homebutton" id="gradient-button">Campuses Directory</Link>
+            <Link to="/students" className="Homebutton" id="gradient-button">Students Directory</Link>
+            {/* <Link to="/" className="Homebutton" id="gradient-button">Home Page</Link> */}
+          </div>
         </div>
+      </div>
     </div>
   );
 };
