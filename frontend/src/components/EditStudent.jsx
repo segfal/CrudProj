81d2424dbd1fd881ab252624de5e6bc41d9b90fe
Hafeefa <hafeefa40@gmail.com>
2023-07-06 12:27:45 -0400

    added email verification

--- frontend/src/components/EditStudent.jsx
+++ frontend/src/components/EditStudent.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SingleStudent from "../pages/SingleStudent";
-import { useParams,useNavigate } from "react-router-dom";
-const EditStudent = () => {
+import { useParams, useNavigate } from "react-router-dom";
 
+const EditStudent = () => {
   const navigate = useNavigate();
   const studentId = useParams();
   const [state, setState] = useState({
@@ -14,60 +14,54 @@ const EditStudent = () => {
     gpa: 0.0,
     campusId: null,
   });
+  const [emailError, setEmailError] = useState("");
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    if(name === "firstName"){
-      setState({
-        ...state,
-        firstName: value,
-      });
-    }
-    if(name === "lastName"){
-      setState({
-        ...state,
-        lastName: value,
-      });
-    }
-    if(name === "email"){
-      setState({
-        ...state,
-        email: value,
-      });
-    }
-    if(name === "imageUrl"){
-      setState({
-        ...state,
-        imageUrl: value,
-      });
-    }
-    if(name === "gpa"){
-      setState({
-        ...state,
-        gpa: value,
-      });
-    }
+    setState({
+      ...state,
+      [name]: value,
+    });
 
+    if (name === "email") {
+      if (!validateEmail(value)) {
+        setEmailError("Invalid email address");
+      } else {
+        setEmailError("");
+      }
+    }
   };
- 
-  //takes student id and updates the student
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    if (!validateEmail(state.email)) {
+      setEmailError("Invalid email address");
+      return;
+    }
+
     console.log(studentId);
-    const response = await axios.put(`http://localhost:8080/routes/students/updatestudent/${studentId.id}`, {
+    const response = await axios.put(
+      `http://localhost:8080/routes/students/updatestudent/${studentId.id}`,
+      {
         firstName: state.firstName,
         lastName: state.lastName,
         email: state.email,
         imageUrl: state.imageUrl,
         gpa: state.gpa,
         campusId: state.campusId,
-    });
-    
+      }
+    );
+
     console.log("response", response);
 
     navigate(`/SingleStudent/${studentId.id}`);
+  };
 
-  }
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
 
   return (
     <div>
@@ -96,10 +90,11 @@ const EditStudent = () => {
           onChange={handleChange}
           placeholder="Enter student e-mail"
         />
+        {emailError && <p className="error">{emailError}</p>}
         <label htmlFor="gpa">GPA</label>
-        <input  
+        <input
           name="gpa"
-          type=""
+          type="text"
           value={state.gpa}
           onChange={handleChange}
           placeholder="Enter student gpa"
@@ -116,7 +111,6 @@ const EditStudent = () => {
       </form>
     </div>
   );
-
 };
 
 export default EditStudent;
