5649446398ca8403c8cd2d6ad1eea1ee8a4471ef
SegFal <92688849+segfal@users.noreply.github.com>
2023-07-07 12:40:46 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = "8080";
 const cors = require('cors');
-const db = require('./db/db');
+const db = require('./db');
 
 //Use Cors to allow cross origin resource sharing
 app.use(cors());
