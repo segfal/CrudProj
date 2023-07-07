aa0726c55c062b96d372e832ddc76b50ac12c16b
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-07-07 03:19:15 -0400

    Express backend

--- backend/DB/db.js
+++ backend/DB/db.js
@@ -1,15 +1,23 @@
 const { Sequelize, DataTypes } = require("sequelize");
-
+const {dotenv} = require('dotenv').config();
 //access postgress database
-const db = new Sequelize("collegeinfo", "postgres", "123", {
-  host: "localhost", //where is the database
-  dialect: "postgres", //what type of database
-  pool: {
-    max: 5, //max number of connections
-    min: 0, //min number of connections
-    require: 30000, //max time to connect
-    idle: 10000, //max time to be connected
-  },
+
+
+const db = new Sequelize(process.env.POSTGRES_URL, {
+    dialectModule: require('pg'),
+    dialect: 'postgres',
+    //ssl true
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
+
+
+
+
+
 
 module.exports = db;
